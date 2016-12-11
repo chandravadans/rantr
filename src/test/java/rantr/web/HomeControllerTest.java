b1af04f88658d2391a3d00ee09c148fb2960cbb9
Chandravadan <s.chandravadan@gmail.com>
2016-12-12 00:42:38 +0530

    Controller test written

---
+++ src/test/java/rantr/web/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package rantr.web;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Created by cv on 12/12/16.
+ */
+public class HomeControllerTest {
+    @Test
+    public void home() throws Exception {
+        HomeController homeController = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+
+}
